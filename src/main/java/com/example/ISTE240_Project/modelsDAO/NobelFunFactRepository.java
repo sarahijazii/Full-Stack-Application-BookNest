6371bc8058ae5291c3aec36532472c2833b57467
jamile <jo4065@rit.edu>
2025-04-20 18:21:44 +0400

    Create NobelFunFactRepository to handle database operations for fun facts

--- src/main/java/com/example/ISTE240_Project/modelsDAO/NobelFunFactRepository.java
+++ src/main/java/com/example/ISTE240_Project/modelsDAO/NobelFunFactRepository.java
@@ -1,9 +1,10 @@
 package com.example.ISTE240_Project.modelsDAO;
 
-
 import com.example.ISTE240_Project.Models.NobelFunFact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+// Repository interface for NobelFunFact entity
 
 @Repository
 public interface NobelFunFactRepository extends JpaRepository<NobelFunFact, Long> {}
