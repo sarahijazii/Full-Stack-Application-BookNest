6233fe5672428cb676a8e31b6e396038b5368971
jamile <jo4065@rit.edu>
2025-04-20 18:24:06 +0400

    Add repository interface for NotableSpeech entity

--- src/main/java/com/example/ISTE240_Project/modelsDAO/NotableRepository.java
+++ src/main/java/com/example/ISTE240_Project/modelsDAO/NotableRepository.java
@@ -4,6 +4,9 @@ import com.example.ISTE240_Project.Models.NotableSpeech;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// repository interface for NotableSpeech entity, it enables interaction
+// with the 'notable_speeches' table
+
 @Repository
 public interface NotableRepository extends JpaRepository<NotableSpeech, Long> {
 }
