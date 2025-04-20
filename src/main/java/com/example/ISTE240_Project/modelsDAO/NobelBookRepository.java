3362a8b229527e6376188158360f3c83ab50485d
jamile <jo4065@rit.edu>
2025-04-20 18:22:43 +0400

    Create NobelBookRepository to manage book-related database operations

--- src/main/java/com/example/ISTE240_Project/modelsDAO/NobelBookRepository.java
+++ src/main/java/com/example/ISTE240_Project/modelsDAO/NobelBookRepository.java
@@ -4,6 +4,7 @@ import com.example.ISTE240_Project.Models.NobelBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// Repository interface for NobelBook entity
 @Repository
 public interface NobelBookRepository extends JpaRepository<NobelBook, Long> {
 
