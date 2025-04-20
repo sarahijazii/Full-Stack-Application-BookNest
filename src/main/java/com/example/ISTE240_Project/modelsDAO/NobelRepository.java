c4ee3ae31138b0e2d5b47a7e8355ca264cf12b28
jamile <jo4065@rit.edu>
2025-04-20 18:10:39 +0400

    Implement NobelPrize repository using Spring Data JPA

--- src/main/java/com/example/ISTE240_Project/modelsDAO/NobelRepository.java
+++ src/main/java/com/example/ISTE240_Project/modelsDAO/NobelRepository.java
@@ -4,7 +4,8 @@ import com.example.ISTE240_Project.Models.NobelPrize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface NobelRepository extends JpaRepository<NobelPrize, Long> {
+// this nobel repository class will extend JpaRepository
+// to provide standard CRUD operations and query capabilities.
 
-}
+@Repository
+public interface NobelRepository extends JpaRepository<NobelPrize, Long> {}
