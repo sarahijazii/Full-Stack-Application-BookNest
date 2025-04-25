0d361103585df43bed6800e6a9d157719676df8a
SaraHijazii <sh9570@rit.edu>
2025-04-26 02:25:20 +0400

    Created initial profile page template and User entity to represent logged in users

--- src/main/java/com/example/ISTE240_Project/Models/Book.java
+++ src/main/java/com/example/ISTE240_Project/Models/Book.java
@@ -6,17 +6,27 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "books")
 public class Book {
-    @Id
+
+    @Id // Primary Key for the Books entity
     private int id;
 
+    @Column
     private String title;
+
+    @Column
     private String author;
+
+    @Column
     private String genre;
+
+    @Column
     private String description;
+
+    @Column
     private String isbn;
 
     @Column(name = "page_count")
-    private Integer pageCount;
+    private int pageCount;
 
     @Column(name = "thumbnail_url")
     private String thumbnailUrl;
@@ -24,13 +34,31 @@ public class Book {
     @Column(name = "publication_date")
     private LocalDate publicationDate;
 
-    public String getThumbnailUrl() {
-        return thumbnailUrl;
-    }
 
+    // GETTERS
+    public int getId() {return id;}
+    public String getIsbn() {return isbn;}
+    public String getTitle() {return title;}
+    public String getAuthor() {return author;}
+    public void setAuthor(String author) {this.author = author;}
+    public String getDescription() {return description;}
+    public String getGenre() {return genre;}
+    public int getPageCount() {return pageCount;}
+    public String getThumbnailUrl() {return thumbnailUrl;}
+    public LocalDate getPublicationDate() {return publicationDate;}
+
+
+    // SETTERS
+    public void setId(int id) {this.id = id;}
+    public void setIsbn(String isbn) {this.isbn = isbn;}
+    public void setTitle(String title) {this.title = title;}
+    public void setDescription(String description) {this.description = description;}
+    public void setGenre(String genre) {this.genre = genre;}
+    public void setPageCount(int pageCount) {this.pageCount = pageCount;}
     public void setThumbnailUrl(String thumbnailUrl) {
         this.thumbnailUrl = thumbnailUrl;
     }
+    public void setPublicationDate(LocalDate publicationDate) {this.publicationDate = publicationDate;}
 
 }
 
---
+++ src/main/java/com/example/ISTE240_Project/Models/User.java
@@ -0,0 +1,72 @@
+package com.example.ISTE240_Project.Models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="USER")
+public class User {
+
+    /* DECLARED FIELDS */
+    @Id
+    private String email;
+
+    @Column(name="Display_Name", length = 50, nullable = false)
+    private String displayName;
+
+    @Column(name="User_Name", length = 50, nullable = false)
+    private String username;
+
+    @Column(name="Password", length = 120, nullable = false)
+    private String password;
+
+    @Column(name="Bio", length = 120, nullable = false)
+    private String bio;
+
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
+    private byte[] image;
+
+    /* GETTERS */
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getBio(){return bio;}
+
+    public byte[] getImage() {return image;}
+
+    /* SETTERS */
+    public void setDisplayName(String displayName) {this.displayName = displayName;}
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBio(String bio){this.bio=bio;}
+
+    public void setImage(byte[] image){this.image=image;}
+
+
+}
